2ca749280d51bb96c6a4ddc6216c55ccd738037a
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2024-05-07 18:03:04 +0530

    updated-text-size-in-movie-card

--- src/Components/MovieSearch/MovieCard.jsx
+++ src/Components/MovieSearch/MovieCard.jsx
@@ -6,14 +6,13 @@ const MovieCard = () => {
             <a href="" className='relative group'>
                 <img src="https://marketplace.canva.com/EAFTl0ixW_k/1/0/1131w/canva-black-white-minimal-alone-movie-poster-YZ-0GJ13Nc8.jpg" alt="" />
                 <div className='absolute bottom-0 p-2 group-hover:bg-gradient-to-t from-black from-15% ... opacity-0 group-hover:opacity-100 duration-200 '>
-                    <h3>Movie Name</h3>
-                    <div>
-                        <p className='text-sm'>10-20-2020</p>
-                        <p className='text-sm'>8.0</p>
+                    <h3 className='text-xs'>Movie Name</h3>
+                    <div className='flex flex-row justify-between'>
+                        <p className='text-ssss text-yellow-300'>10-20-2020</p>
+                        <p className='text-ssss text-yellow-300'>8.0</p>
                     </div>
-                    <p className='text-xs mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione delectus a ducimus in magnam est!</p>
+                    <p className='text-ss mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione delectus a ducimus in magnam est!</p>
                 </div>
-
             </a>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       fontFamily: {
         sans: ['Bai Jamjuree', 'sans-serif'],
         inter: ['Inter'],
+      },
+      fontSize: {
+        s: '0.4rem',
+        ss: '0.5rem',
+        sss: '0.6rem',
+        ssss: '0.7rem'
       }
     },
   },
